daffb7d10f77904ff557854171a5c4aec526dc7d
ihar.velichkevich <igor.velichkevich@gmail.com>
2020-10-09 21:11:14 +0800

    chage colors

--- config.def.h
+++ config.def.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 //static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
-static char *font = "UbuntuMono Nerd Font:pixelsize=12:antialias=true:autohint=true";
+static char *font = "UbuntuMono Nerd Font:pixelsize=14:antialias=true:autohint=true";
 static int borderpx = 4;
 
 /*
@@ -97,24 +97,24 @@ unsigned int tabspaces = 8;
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
 	/* 8 normal colors */
-	"black",
-	"red3",
+	"#003440", // "black",
+	"#893E3E", //"red3", 
 	"green3",
 	"yellow3",
-	"blue2",
-	"magenta3",
-	"cyan3",
-	"gray90",
+	"#268ad2",
+	"#b67dc1",
+	"#2aa197",
+	"gray90", // main text
 
 	/* 8 bright colors */
 	"gray50",
-	"red",
-	"green",
+	"#893E3E", //"red",
+	"#7cc67f",
 	"yellow",
-	"#5c5cff",
-	"magenta",
-	"cyan",
-	"white",
+	"#268ad2",
+	"#b67dc1",
+	"#2aa197",
+	"green3",
 
 	[255] = 0,
 

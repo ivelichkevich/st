6ac4a28d15a68b3ab52bf3ea4f982b9a2ae1e9ff
ihar.velichkevich <igor.velichkevich@gmail.com>
2021-07-03 13:29:48 +0800

    clip

--- x.c
+++ x.c
@@ -712,10 +712,7 @@ brelease(XEvent *e)
 
 	if (mouseaction(e, 1))
 		return;
-	if (e->xbutton.button == Button1) {
-		mousesel(e, 1);
-		//openUrlOnClick(evcol(e), evrow(e), url_opener); //todo crash on select archVm in prompt
-    }
+	if (e->xbutton.button == Button1) { mousesel(e, 1); }
 }
 
 void

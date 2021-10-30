0d1569ebeab33ef7b157f79c2bdc8c3d9c9c63a5
Ihar Velichkevich <igor.velichkevich@gmail.com>
2021-10-30 21:34:22 +0800

    install.sh

---
+++ install.sh
@@ -0,0 +1,7 @@
+yay -S libxft-bgra # for emoji
+sudo pacman -S xclip     # copy to clipboard in term
+yay -S mimeo  # open files by type, settings in vim /home/ihar/.config/mimeo/associations.txt
+yay -S xdg-utils-mimeo # replace xdg-open with mimeo (xdg  1.1.3-3 bugs if use in ST)
+
+sudo make && sudo make clean install
+
